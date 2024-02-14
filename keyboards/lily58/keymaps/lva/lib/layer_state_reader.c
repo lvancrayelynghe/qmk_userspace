533674737a1ff02c47627849051d7df57cc18fc9
Luc Vancrayelynghe <luc.vancrayelynghe@gmail.com>
2024-02-15 00:20:16 +0100

    Try to fix layer name on master OLED screen

--- keyboards/lily58/keymaps/lva/lib/layer_state_reader.c
+++ keyboards/lily58/keymaps/lva/lib/layer_state_reader.c
@@ -5,41 +5,39 @@
 #define L_SHIFTED (1 << 1)
 #define L_LOWER (1 << 2)
 #define L_RAISE (1 << 3)
-#define L_SUPER (1 << 4)
-#define L_ADJUST (1 << 5)
-#define L_ADJUST_TRI (L_ADJUST | L_RAISE | L_LOWER)
-#define L_FUNC (1 << 6)
+#define L_ADJUST (1 << 4)
+#define L_HYPER (1 << 5)
+#define L_SUPER (1 << 6)
 
 char layer_state_str[24];
 
 const char *read_layer_state(void) {
     switch (layer_state)
     {
-    case L_BASE:
-        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: BASE");
-        break;
-    case L_SHIFTED:
-        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: SHIFTED");
-        break;
-    case L_LOWER:
-        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: LOWER");
-        break;
-    case L_RAISE:
-        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: RAISE");
-        break;
-    case L_SUPER:
-        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: SUPER");
-        break;
-    case L_ADJUST:
-    case L_ADJUST_TRI:
-        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: ADJUST");
-        break;
-    case L_FUNC:
-        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: FUNCTION");
-        break;
-    default:
-        snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%u", layer_state);
+        case L_BASE:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: BASE");
+            break;
+        case L_SHIFTED:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: SHIFTED");
+            break;
+        case L_LOWER:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: LOWER");
+            break;
+        case L_RAISE:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: RAISE");
+            break;
+        case L_ADJUST:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: ADJUST");
+            break;
+        case L_HYPER:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: HYPER");
+            break;
+        case L_SUPER:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: SUPER");
+            break;
+        default:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%u", layer_state);
     }
 
-  return layer_state_str;
+    return layer_state_str;
 }
